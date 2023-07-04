f927a87f01c35c138f4e113460da631b94ba8e37
Davi Alexandre <deltreertl@gmail.com>
2023-07-04 03:32:01 -0300

    ts

--- experimenting.js
+++ experimenting.js
@@ -1,92 +1,112 @@
 import _ from 'lodash';
-import { set, get } from './setGet.js';
-
+import { set } from './setGet.js';
 /** from object to path-value pairs */
-function toLeaves(ob, mapLeaf = (x) => x) {
-  // flatten
-  const toReturn = {};
-  _.forEach(ob, (value, key) => {
-    if (!_.isObject(value)) {
-      toReturn[key] = value; // leaf
-      return;
+function toLeaves(obj, mapLeaf = (x, _) => x) {
+    // flatten
+    function flatten(ob) {
+        const result = {};
+        _.forEach(ob, (value, key) => {
+            if (!_.isObject(value)) {
+                result[key] = value; // leaf
+                return;
+            }
+            const flat = flatten(value);
+            _.forEach(flat, (subValue, subKey) => {
+                const k = key + (_.isArray(value) ? `[${subKey}` : `.${subKey}`);
+                result[k] = subValue;
+            });
+        });
+        return result;
     }
-    const flat = toLeaves(value);
-    _.forEach(flat, (subValue, subKey) => {
-      const arr = _.isArray(value);
-      const k = key + (arr ? `[${subKey}]` : `.${subKey}`);
-      toReturn[k] = subValue;
+    const toReturn = flatten(obj);
+    // map
+    _.forEach(toReturn, (value, path) => {
+        const str = path.replace(/\[[0-9]*/g, '[1]');
+        console.log(str);
+        toReturn[str] = mapLeaf(value, path);
     });
-  });
-  // map
-  _.forEach(toReturn, (value, path) => {
-    toReturn[path] = mapLeaf(value, path);
-  });
-  return toReturn;
+    return toReturn;
 }
-
 /** from path-value pairs to object */
-function toTree(obj, mapLeaf = (x) => x) {
-  let toReturn = {};
-  _.forEach(obj, (value, path) => {
-    // setPathValue(toReturn, path, mapLeaf(value, path));
-    set(toReturn, path, mapLeaf(value, path));
-  });
-  return toReturn;
+function toTree(obj, mapLeaf = (x, _) => x) {
+    let toReturn = {};
+    _.forEach(obj, (value, path) => {
+        // setPathValue(toReturn, path, mapLeaf(value, path));
+        set(toReturn, path, mapLeaf(value, path));
+    });
+    return toReturn;
 }
-
 const ob1 = {
-  a: {
-    b: ['davi', 'alexandre'],
-    c: 'null',
-  },
-  x: {
-    y: 'yplson',
-  },
+    a: {
+        b: ['davi', 'alexandre'],
+        c: 'null',
+    },
+    x: {
+        y: 'yplson',
+    },
 };
-
 const ob2 = {
-  a: {
-    b: {
-      0: 'davi',
-      1: 'alexandre',
+    a: {
+        b: {
+            0: 'davi',
+            1: 'alexandre',
+        },
+        c: null,
     },
-    c: null,
-  },
-  x: {
-    y: 'yplson',
-  },
+    x: {
+        y: 'yplson',
+    },
 };
-
 const ob3 = {
-  a: {
-    b: {
-      0: 'davi',
-      one: 'alexandre',
+    a: {
+        b: {
+            0: 'davi',
+            one: 'alexandre',
+        },
+        c: null,
     },
-    c: null,
-  },
-  x: {
-    y: 'yplson',
-  },
+    x: {
+        y: 'yplson',
+    },
 };
-
-function test(ob) {
-  const record = toLeaves(ob);
-  const inversed = toTree(record);
-  console.log(JSON.stringify(ob, null, 0));
-  console.log(JSON.stringify(record, null, 0));
-  console.log(JSON.stringify(inversed, null, 0));
+function test(tree) {
+    const leaves = toLeaves(tree);
+    const inversed = toTree(leaves);
+    const str = (v) => JSON.stringify(v, null, 0);
+    console.log(str(tree));
+    console.log(JSON.stringify(leaves, null, 2));
+    // console.log(str(inversed));
+    console.log(str(tree) === str(inversed));
 }
-
-test(ob2);
-
+const ob = {
+    id: '0001',
+    type: 'donut',
+    name: 'Cake',
+    ppu: 0.55,
+    batters: {
+        batter: [
+            [[[[[{ id: '1001', type: 'Regular' }]]]]],
+            { id: '1002', type: 'Chocolate' },
+            { id: '1003', type: 'Blueberry' },
+            { id: '1004', type: "Devil's Food" },
+        ],
+    },
+    topping: [
+        { id: '5001', type: 'None' },
+        { id: '5002', type: 'Glazed' },
+        { id: '5005', type: 'Sugar' },
+        { id: '5007', type: 'Powdered Sugar' },
+        { id: '5006', type: 'Chocolate with Sprinkles' },
+        { id: '5003', type: 'Chocolate' },
+        { id: '5004', type: 'Maple' },
+    ],
+};
+test(ob);
 // console.log(_.toPath('a[0].2.f'));
-
 // The problem with lodash's toPath is that it gives
 // the same output for both 'a[0]' and 'a.0'
 // and for that reason, it can't tell arrays and objects apart
 // which is a problem if you want to inverse the path record and use the array's methods
 // another problem arises if you mix numbers and words as keys
-
 // a[0].b = ... ==>  {a: [   {b: ...} ]}
 // a.0.b  = ... ==>  {a: {0: {b: ...} }}
---
+++ setGet.ts
@@ -0,0 +1,62 @@
+import _ from 'lodash';
+
+/** returns the property value at path on a given object */
+export const get = (obj, path) => _.property(path)(obj);
+
+/** in-place set value of a deep nested value */
+export function set(obj, path, value) {
+  // split by dots or bracket pairs, except when preceded by backslash
+  const points = /(?<!\\)\./;
+  const brackets = /(?<!\\)\[(.*?)(?<!\\)\]/;
+  // a[1][2].b ==> [['a', '1', '2'], ['b']]
+  // group keys by whether they refer to directly nested array or object keys
+  // --> object key   ==> single-element array, with the key
+  // --> arr[][][]... ==> > 2 elements; the last one is an object key
+  const groups: any[] = [];
+  for (const sub of path.split(points)) {
+    const match = sub.split(brackets);
+    const len = match.length;
+    // .any.
+    if (len === 1) {
+      groups.push([match[0]]);
+      continue;
+    }
+    // .any[number].
+    const [key, ...indices] = match.filter((x) => x);
+    if (indices.length < 1)
+      throw new Error(
+        `The notation 'prop.[any]' is not supported
+        Use 'prop[number]' or 'prop.any' instead`,
+      );
+    const keys = [key];
+    for (const i of indices) {
+      if (isNaN(i))
+        throw new Error(
+          `The notation 'prop[string]' is not supported, only for numbers, when 'prop' is an array.
+          Use 'prop.string' instead.`,
+        );
+      keys.push(i);
+    }
+    groups.push(keys);
+  }
+  // [a]        => {a: {}}
+  // [a, 1, 0]  => {a: [ ___, [ {} ] ]}
+  let ref = obj;
+  for (const group of groups) {
+    const isLastGroup = group === _.last(groups);
+    if (group.length === 1) {
+      if (isLastGroup) break;
+      ref[group[0]] ??= {};
+      ref = ref[group[0]];
+      continue;
+    }
+    for (const key of group) {
+      const isLastKey = key === _.last(group);
+      if (isLastGroup && isLastKey) break;
+      ref[key] ??= isLastKey ? {} : [];
+      ref = ref[key];
+    }
+  }
+  ref[groups.flat().slice(-1)[0]] = value;
+  return obj;
+}
