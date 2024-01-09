b6162e5c717381b85b4c9d05208d98323dabd6ad
Davi Alexandre <deltreertl@gmail.com>
2024-01-09 17:33:52 -0300

    walkLeaves yields [path, value, keys]

--- src/toLeaves.ts
+++ src/toLeaves.ts
@@ -3,38 +3,47 @@ import { Leaves, Leaf } from './Leaves.js';
 
 /** Create a path-value pair record of all the leaf values within an object */
 export default function toLeaves<T extends Leaf>(obj: object): Leaves<T> {
-  const toReturn = _.fromPairs([...walkLeaves(obj)]);
+  const leaves = [...walkLeaves(obj)].map(([a, b]) => [a, b]);
+  const toReturn = _.fromPairs(leaves);
   return toReturn;
 }
 
 export function* walkLeaves(ob: object) {
-  const keys: string[] = [];
+  const paths: string[] = [];
   const generators = [generate(ob)];
+  const properties: Property[] = [];
 
   while (generators.length > 0) {
     const currGenerator = _.last(generators)!;
     const entry = currGenerator.next();
     if (entry.done) {
       generators.pop();
-      keys.pop();
+      paths.pop();
+      properties.pop();
       continue;
     }
 
-    const [key, value] = entry.value;
+    const [path, value, prop] = entry.value;
     if (_.isObject(value)) {
       generators.push(generate(value));
-      keys.push(key);
+      paths.push(path);
+      properties.push(prop);
     } else {
-      yield [[...keys, key].join(''), value];
+      yield [[...paths, path].join(''), value, [...properties, prop]];
     }
   }
 }
 
-function* generate(ob: object) {
+type Property = {
+  name: string;
+  isArrayIndex: boolean;
+};
+
+function* generate(ob: object): Generator<[string, object | Leaf, Property]> {
   for (const [key, value] of Object.entries(ob)) {
-    let k = _.isArray(ob) ? `[${key}]` : key;
+    let path = _.isArray(ob) ? `[${key}]` : key;
     // [num].prop || prop1.prop2 <=> child is object and not array
-    if (_.isObject(value) && !_.isArray(value)) k += '.';
-    yield [k, value];
+    if (_.isObject(value) && !_.isArray(value)) path += '.';
+    yield [path, value, { name: key, isArrayIndex: _.isArray(ob) }];
   }
 }
