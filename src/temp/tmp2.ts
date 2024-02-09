d200036dedf22a0b69bbc60de1a241f0ce7f796c
Davi Alexandre <deltreertl@gmail.com>
2024-02-09 20:44:42 -0300

    update incremental suggestion

--- src/temp/tmp2.ts
+++ src/temp/tmp2.ts
@@ -13,32 +13,34 @@ interface C {
   VALOR: 2;
 }
 
-type LastKey<T extends string> = T extends `${string}.${infer rest}`
-  ? LastKey<rest>
-  : T;
+// [path, circular reference]
+type Cycle = readonly [string, object];
 
 declare function p<
-  const VALUE extends string | object,
-  const PATH extends Extract<VALUE, string> = Extract<VALUE, string>,
-  const REF extends Extract<VALUE, object> = Extract<VALUE, object>,
-  const SUBPATH extends Extract<LeafPath<REF>, string> = Extract<
-    LeafPath<REF>,
+  const RETURNED extends Cycle | string,
+  // Extract information
+  const CYCLES extends Extract<RETURNED, Cycle> = Extract<RETURNED, Cycle>,
+  const LEAF_PATHS extends Extract<RETURNED, string> = Extract<
+    RETURNED,
     string
   >,
-  const CLEAN_PATH extends RemoveInterpolator<PATH> = RemoveInterpolator<PATH>,
+  // Map leaf-paths to tuple with null circular ref ( [path, null] )
+  const PATH_REF_PAIR extends
+    | CYCLES
+    | { [P in LEAF_PATHS]: [P, null] }[LEAF_PATHS] =
+    | CYCLES
+    | { [P in LEAF_PATHS]: [P, null] }[LEAF_PATHS],
+  // Create path
+  const PATH extends CYCLES[0] = CYCLES[0],
+  const REF extends CYCLES[1] = CYCLES[1],
+  const SUBPATH extends Extract<LeafPath<REF>, Cycle>[0] = Extract<
+    LeafPath<REF>,
+    Cycle
+  >[0],
+  const CLEAN_PATH extends PATH = PATH,
   const EXTENDED_PATH extends `${CLEAN_PATH}.${SUBPATH}` = `${CLEAN_PATH}.${SUBPATH}`,
-  const CLEAN_EXTENDED_PATH extends RemoveInterpolator<EXTENDED_PATH> = RemoveInterpolator<EXTENDED_PATH>,
->(path: CLEAN_PATH | CLEAN_EXTENDED_PATH): VALUE;
+  const CLEAN_EXTENDED_PATH extends EXTENDED_PATH = EXTENDED_PATH,
+>(path: CLEAN_PATH | CLEAN_EXTENDED_PATH): RETURNED;
 
-// won't work as is for union of circular + non circular strings
-// type RemoveInterpolatorAny<
-//   T extends string,
-//   ACC extends string = '',
-// > = T extends `${infer CHAR}${infer REST}`
-//   ? REST extends ''
-//     ? ACC
-//     : RemoveInterpolatorAny<REST, `${ACC}${CHAR}`>
-//   : never;
-type RemoveInterpolator<T> = T;
-
-const a: LeafPath<A> = p('b.c.a');
+const a: LeafPath<A> = p('');
+const b: LeafPath<A> = 'b.c.VALOR';
