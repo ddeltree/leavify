e07a179b654c8e34aff235dd68ce172cfc81cdd3
Davi Alexandre <deltreertl@gmail.com>
2024-03-05 09:57:44 -0300

    refactor

--- src/__tests__/newSetter.test.ts
+++ src/__tests__/newSetter.test.ts
@@ -1,14 +1,31 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable  */
 import { test, expect } from 'vitest';
-import { getBindings, isNoCollisionSubPath } from '../newSetter.js';
+import {
+  createMissingRefs,
+  getBindings,
+  hasTypeCollision,
+} from '../newSetter.js';
 import { split } from '../parsePath.js';
+import LeafPath from '../types/LeafPath.js';
 
 test('only root key', () => {
-  const ref1: any = { a: [2, [2]] };
-  const dotPath1 = split('a[1][1]')[0];
+  const ref1 = { a: [2, [2]] } as const;
+  // console.log(getBindings(ref1, dotPath1));
 
-  console.log(getBindings(ref1, dotPath1));
-  expect(isNoCollisionSubPath(ref1, dotPath1)).toBe(true);
+  const isValid = (path: (string & {}) | LeafPath<typeof ref1>) =>
+    hasTypeCollision(ref1 as any, getBindings(ref1, split(path)[0]));
+  const createMissing = (path: (string & {}) | LeafPath<typeof ref1>) =>
+    createMissingRefs(getBindings(ref1, split(path)[0]));
+  console.log(createMissing('a[0][]'));
+  expect(() => isValid('')).toThrowError();
+  expect(isValid('other key')).toBe(true);
+  expect(isValid('a')).toBe(false);
+  expect(isValid('a[]')).toBe(true);
+  expect(isValid('a[][]')).toBe(false);
+  expect(isValid('a[1][]')).toBe(true);
+  expect(isValid('a[1][1]')).toBe(true);
+  expect(isValid('a[2]')).toBe(true);
+  expect(isValid('a[2][][42]')).toBe(true);
 });
 
 // test('only array index', () => {
--- src/newSetter.ts
+++ src/newSetter.ts
@@ -1,12 +1,13 @@
+/* eslint-disable */
 import _ from 'lodash';
 import { SubPath as SubPath, split } from './parsePath.js';
 import { Primitive } from './types/Leaves.js';
 
-export function reconstruct(root: Ref, dotPath: SubPath) {
-  const refs = createMissingRefs(root, dotPath)
-    .slice(1) // remove root ref
-    .filter((x) => x !== undefined);
-  const keys = [dotPath.key, ...(dotPath.indices ?? [])].filter(
+export function safeReconstruct(root: Root, subPath: SubPath) {
+  const bindings = getBindings(root as any, subPath);
+  if (hasTypeCollision(root, bindings)) throw new Error();
+  const refs = createMissingRefs(bindings).filter((x) => x !== undefined);
+  const keys = [subPath.key, ...(subPath.indices ?? [])].filter(
     (x) => x !== undefined,
   );
   if (keys.length !== refs.length) throw new Error();
@@ -18,19 +19,8 @@ export function reconstruct(root: Ref, dotPath: SubPath) {
   return root;
 }
 
-export function createMissingRefs(refs: BindingRefs) {
-  if (hasTypeCollision(refs)) throw new Error();
-  if (refs.keyRef) refs.keyRef[1] ??= {};
-  if (refs.indexRefs) {
-    for (let i = 0; i < refs.indexRefs.length - 2; i++)
-      refs.indexRefs[i][1] ??= [];
-    refs.indexRefs[refs.indexRefs.length - 1] = {};
-  }
-  return refs;
-}
-
-export function getBindings(rootRef: Extract<Ref, object>, dotPath: SubPath) {
-  const keys = [dotPath.key, ...(dotPath.indices ?? [])].filter(
+export function getBindings(rootRef: Root, subPath: SubPath) {
+  const keys = [subPath.key, ...(subPath.indices ?? [])].filter(
     (x) => x !== undefined,
   ) as [string, ...number[]] | number[];
   const refs = keys
@@ -39,21 +29,36 @@ export function getBindings(rootRef: Extract<Ref, object>, dotPath: SubPath) {
   return refs.map((ref, i) => [keys[i], ref]);
 }
 
-export function isNoCollisionSubPath(
-  rootRef: Extract<Ref, object>,
-  dotPath: SubPath,
-) {
-  const bindings = getBindings(rootRef, dotPath);
+function getIndex(ob: Ref, key: string | number) {
+  return !ob || typeof ob !== 'object' || !Object.hasOwn(ob, key) ?
+      undefined
+    : (ob[key as any] as any);
+}
+
+export function createMissingRefs(bindings: BindingRefs) {
+  if (typeof _.first(bindings) === 'string') bindings[0][1] ??= {};
+  for (let i = 0; i < bindings.length - 1; i++) {
+    bindings[i][1] ??= [];
+    // override leaf values along the way:
+    // TODO validate for type collision
+    if (!_.isObject(bindings[i][1])) bindings[i][1] = [];
+  }
+  bindings[bindings.length - 1][1] ??= {};
+  return bindings;
+}
+
+export function hasTypeCollision(root: Root, bindings: BindingRefs) {
   const refs = bindings.map(([_, ref]) => ref);
   const firstKey = bindings[0][0];
   let isValid = true;
 
   // ROOT
   if (typeof firstKey === 'string') {
-    isValid &&= _.isObject(rootRef) && !_.isArray(rootRef);
+    isValid &&= _.isObject(root) && !_.isArray(root);
   } else if (typeof firstKey === 'number') {
-    isValid &&= _.isArray(rootRef);
+    isValid &&= _.isArray(root);
   }
+
   // TRUNK
   isValid &&= refs
     .slice(1, -1)
@@ -64,12 +69,8 @@ export function isNoCollisionSubPath(
   return isValid;
 }
 
-function getIndex(ob: Ref, key: string | number) {
-  return !ob || typeof ob !== 'object' || !Object.hasOwn(ob, key) ?
-      undefined
-    : (ob[key as any] as any);
-}
-
+type Root = Extract<Ref, object>;
+type BindingRefs = ReturnType<typeof getBindings>;
 type Ref = UnknownArray | UnknownRecord | Primitive;
 type UnknownArray = Array<Ref>;
 type UnknownRecord = Record<string | number | symbol, UnknownArray | Primitive>;
