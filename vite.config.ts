027385ebc2745c76e08fcffd9a190a2ae3a9c805
Davi Alexandre <deltreertl@gmail.com>
2024-04-14 20:16:12 -0300

    chore: configure path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,18 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import { configDefaults } from 'vitest/config';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   esbuild: {
     target: 'es2020',
   },
+
   test: {
     watch: false,
     coverage: {
       exclude: [...configDefaults.exclude, 'src/types'],
     },
   },
+  plugins: [tsconfigPaths()],
 });
