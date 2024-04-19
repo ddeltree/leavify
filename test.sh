44ff900a62be41f7e5926955cd7b838c856d3b7d
Davi Alexandre <deltreertl@gmail.com>
2024-04-18 21:39:42 -0300

    test: refactor test scripts

--- test.sh
+++ test.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
-if ! [ -d node_modules/leavify ]; then
-  npm link;
-  npm link leavify;
+# TYPES
+if [[ " $@ " =~ " --types " ]]; then
+  if [ ! -d dist ]; then
+    mkdir dist;
+  fi
+  touch dist/index.d.ts;
+  tsd --files __tests__/types/**/*.test-d.ts;
+# BUILD PACKAGE THEN TEST
+elif [[ " $@ " =~ " --package " ]]; then
+  npm run build
+  if ! [ -d node_modules/leavify ]; then
+    npm link;
+    npm link leavify;
+  fi
+  args=( "$@" )
+  for (( i=0; i<"${#args[@]}"; i++ )); do
+    if [[ "${args[i]}" == "--package" ]]; then
+      unset "args[i]"
+      break
+    fi
+  done
+  vitest "${args[@]}" --dir __tests__/integration;
+# TEST WITHOUT BUILDING
+else
+  vitest "$@" --exclude __tests__/integration;
 fi
-vitest "$@";
