f823d435f49fc91ecb0d435b7badbc54c11bfca3
Davi Alexandre <deltreertl@gmail.com>
2024-06-21 18:14:54 -0300

    refactor(walkLeaves): add `Branch` class

--- src/accessors/walkLeaves.ts
+++ src/accessors/walkLeaves.ts
@@ -4,29 +4,56 @@ import { Primitive, LeafPath } from '@typings';
 export default function* walkLeaves<T extends object>(
   target: T,
 ): Generator<readonly [LeafPath<T>, Primitive], undefined> {
-  const branch = [makeChildEntryGenerator(target)];
-  const nodeValues: object[] = [target];
-  const nodeKeys: string[] = [];
-  while (branch.length > 0) {
-    const innerNode = _.last(branch)!;
-    const child = innerNode.next();
+  const branch = new Branch(target);
+  while (!branch.isDone()) {
+    const child = branch.getNextChild();
     if (child.done) {
       branch.pop();
-      nodeValues.pop();
-      nodeKeys.pop();
       continue;
     }
     const [key, value] = child.value;
     if (_.isObject(value)) {
-      if (nodeValues.includes(value)) {
-        continue;
-      }
-      nodeValues.push(value);
-      branch.push(makeChildEntryGenerator(value));
-      nodeKeys.push(key);
+      branch.push([key, value]);
     } else {
-      yield [[...nodeKeys, key].join('') as LeafPath<T>, value];
+      yield [(branch.toString() + key) as LeafPath<T>, value];
     }
+  }
+}
+
+class Branch {
+  private readonly keys: string[] = [];
+  private readonly values: object[];
+  private readonly childrenIterators: Generator<
+    readonly [string, object | Primitive],
+    undefined
+  >[];
+  constructor(target: object) {
+    this.childrenIterators = [makeChildEntryGenerator(target)];
+    this.values = [target];
+  }
+  has(value: object) {
+    return this.values.includes(value);
+  }
+  push([key, value]: [string, object]) {
+    if (this.values.includes(value))
+      throw new Error('Unexpected circular reference!');
+    this.keys.push(key);
+    this.values.push(value);
+    this.childrenIterators.push(makeChildEntryGenerator(value));
+  }
+  pop() {
+    this.keys.pop();
+    this.values.pop();
+    this.childrenIterators.pop();
+  }
+  getNextChild() {
+    return _.last(this.childrenIterators)!.next();
+  }
+  isDone() {
+    return this.childrenIterators.length == 0;
+  }
+  toString() {
+    return this.keys.join('');
   }
 }
 
